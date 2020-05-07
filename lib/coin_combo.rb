0a56eafe88360910f2bc547f6c33ac839d4df520
austinb847 <austinb847@icloud.com>
2020-05-07 11:16:05 -0700

    meaningful fail test for coin_list method

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -4,5 +4,15 @@ class CoinCalculator
   def initialize(total_cents)
     @total_cents = total_cents
   end
+
+  def coin_list()
+    # return [
+    #   0.50,
+    #   0.25,
+    #   0.10,
+    #   0.05,
+    #   0.01
+    # ]
+  end
   
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -7,4 +7,17 @@ describe("CoinCalculator#generate_coin_combo") do
     cents = CoinCalculator.new(0.06)
     expect(cents.total_cents).to(eq(0.06))
   end
+
+  # it("reduces the coins down to only the coins that are less than the total_cents") do
+  #   cents = CoinCalculator.new(0.06)
+
+  # end
+end
+
+describe("CoinCalculator#coin_list") do
+  it("correctly returns a list of coin values") do
+    cents = CoinCalculator.new(0.06)
+    expect(cents.coin_list()).to(eq([0.50, 0.25, 0.10, 0.05, 0.01]))
+  end
+
 end
